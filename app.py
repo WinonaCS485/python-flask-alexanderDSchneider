a5cfc5fe89e7c984fae45ca0f4f0cf1c0ff49229
alexanderDSchneider <schneideralexander78@gmail.com>
2020-02-03 22:26:15 -0600

    Made required changes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pymysql.cursors
 
 app = Flask(__name__)
 
@@ -10,5 +11,43 @@ def index():
 def cakes():
     return 'Yummy cakes!'
 
+@app.route('/hello/<name>')
+def hello(name):
+    return render_template('page.html', name=name)
+
+@app.route('/db')
+def display_db():
+    connection = pymysql.connect(host='mrbartucz.com',
+                             user='eb1391ck',
+                             password='putty1562',
+                             db='eb1391ck_university',
+                             charset='utf8mb4',
+                             cursorclass=pymysql.cursors.DictCursor)
+    
+
+    try:
+        with connection.cursor() as cursor:
+            # Select all Students
+            sql = "SELECT * from Student"
+        
+           # execute the SQL command
+            cursor.execute(sql)
+            
+            #for result in cursor:
+                #print(result)
+            # get the results
+            data = cursor.fetchall()
+            
+            return render_template('db.html', output_data = data)
+      
+            # If you INSERT, UPDATE or CREATE, the connection is not autocommit by default.
+            # So you must commit to save your changes. 
+            # connection.commit()
+        
+
+    finally:
+        connection.close()
+           
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, port=1391)
